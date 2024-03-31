3ef2720d199dacf3caa0c6a7cbebee37818ee548
hw@chill <hw56@indiana.edu>
2024-03-31 00:44:25 -0400

    feat: add checkpoint for debugging

--- evaluate_outputs.py
+++ evaluate_outputs.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
             output = tokenizer.decode(
                 outputs[0], clean_up_tokenization_spaces=True, skip_special_tokens=True
             )
-            print(output)
+            print(output.cpu())
             result = calculate_metrics(
                 generated_text=output,
                 target_text=example["target"],
