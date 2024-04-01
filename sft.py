e97c1c03964fdef4628b73a22cfd8c92d2f998c5
hw@chill <hw56@indiana.edu>
2024-04-01 13:31:47 -0400

    feat: add sft scripts fro llama2

---
+++ sft.py
@@ -0,0 +1,56 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_from_disk
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
+from utils import DATASET_PATH, SEED
+
+MODEL_NAME = 'meta-llama/Llama-2-7b-hf'
+RESPONSE_TEMP = "### Answer:"
+
+def formatting_func(example):
+    output_texts = []
+    for i in range(len(example['instruction'])):
+        text = (f"### Please simplify the scholarly abstract so it is immediately understandable to a layperson: {example['source'][i]}\n {RESPONSE_TEMP} {example['target'][i]}")
+        output_texts.append(text)
+    return output_texts
+
+collator = DataCollatorForCompletionOnlyLM(RESPONSE_TEMP, tokenizer=tokenizer)
+
+
+if __name__ == "__main__":
+    torch.manual_seed(SEED)
+
+    dataset = load_from_disk(DATASET_PATH)
+    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.bfloat16)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    trainer = SFTTrainer(
+        model,
+        args={'output_dir': 'ckpts/sft_llama2-7b-hf',
+              'overwrite_output_dir': False,
+              'do_train': True,
+              'do_eval': True,
+              'do_predict': True,
+              'evaluation_strategy': 'steps',
+              'per_device_train_batch_size': 4,
+              'per_device_eval_batch_size': 4,
+              'gradient_accumulation_steps': 4,
+              'learning_rate': 3e-5,
+              'weight_decay': 1e-1,
+              'logging_steps': 20,
+              'eval_steps': 200,
+              'bf16': True,
+              'run_name': 'sft_llama2-7b',
+              'report_to': 'wandb',
+              'load_best_model_at_end': True,
+              'save_steps': 200,
+              'save_total_limit': 3
+
+              },
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['validation'],
+        formatting_func=formatting_func,
+        data_collator=collator,
+    )
+
+    trainer.train()
