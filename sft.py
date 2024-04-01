a66c3f5e9a0ac090c67d0c77825829cf68b64aee
hw@chill <hw56@indiana.edu>
2024-04-01 14:43:25 -0400

    fix: trainer's arg

--- sft.py
+++ sft.py
@@ -16,14 +16,17 @@ collator = DataCollatorForCompletionOnlyLM(RESPONSE_TEMP, tokenizer=tokenizer)
 #     for i in range(len(example['instruction'])):
 #         text = (
 #             f"### Please simplify the scholarly abstract so it is immediately "
-#             f"understandable to a layperson: {example['source'][i]}\n {RESPONSE_TEMP} "
+#             f"understandable to a layperson: {example['source'][i]}\n {
+#             RESPONSE_TEMP} "
 #             f"{example['target'][i]}")
 #         output_texts.append(text)
 #     return output_texts
 
 
 def formatting_func(example):
-    text = f"### Please simplify the scholarly abstract so it is immediately understandable to a layperson: {example['source']}\n {RESPONSE_TEMP} {example['target']}"
+    text = (f"### Please simplify the scholarly abstract so it is immediately "
+            f"understandable to a layperson: {example['source']}\n {RESPONS
+            E_TEMP} {example['target']}")
     return text
 
 
@@ -33,34 +36,36 @@ if __name__ == "__main__":
     dataset = load_from_disk(DATASET_PATH)
     model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype=torch.bfloat16)
 
+    common_args = {'output_dir': 'ckpts/sft_llama2-7b-hf',
+                   'overwrite_output_dir': False,
+                   'do_train': True,
+                   'do_eval': True,
+                   'do_predict': True,
+                   'evaluation_strategy': 'steps',
+                   'per_device_train_batch_size': 4,
+                   'per_device_eval_batch_size': 4,
+                   'gradient_accumulation_steps': 4,
+                   'learning_rate': 3e-5,
+                   'weight_decay': 1e-1,
+                   'logging_steps': 20,
+                   'eval_steps': 200,
+                   'bf16': True,
+                   'run_name': 'sft_llama2-7b',
+                   'report_to': 'wandb',
+                   'load_best_model_at_end': True,
+                   'save_steps': 200,
+                   'save_total_limit': 3,
+                   'remove_unused_columns': True
+                   }
+
     trainer = SFTTrainer(
         model,
-        args={'output_dir': 'ckpts/sft_llama2-7b-hf',
-              'overwrite_output_dir': False,
-              'do_train': True,
-              'do_eval': True,
-              'do_predict': True,
-              'evaluation_strategy': 'steps',
-              'per_device_train_batch_size': 4,
-              'per_device_eval_batch_size': 4,
-              'gradient_accumulation_steps': 4,
-              'learning_rate': 3e-5,
-              'weight_decay': 1e-1,
-              'logging_steps': 20,
-              'eval_steps': 200,
-              'bf16': True,
-              'run_name': 'sft_llama2-7b',
-              'report_to': 'wandb',
-              'load_best_model_at_end': True,
-              'save_steps': 200,
-              'save_total_limit': 3,
-              'remove_unused_columns': None
-              },
         train_dataset=dataset['train'],
         eval_dataset=dataset['validation'],
         formatting_func=formatting_func,
         data_collator=collator,
         max_seq_length=1024,
+        **common_args
     )
 
     trainer.train()
