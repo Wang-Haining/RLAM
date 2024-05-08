1e880e0dcb5f8f8fe4adc9e31bb1430275896e37
hw@chill <hw56@indiana.edu>
2024-05-08 16:53:19 -0400

    fix: remove misc param from ppo_config

--- ppo.py
+++ ppo.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
     config_kwargs = vars(args).copy()
     keys_to_pop = [
         "sent_len_reward_coef",
-        "word_difficulty_reward_coef"
+        "word_difficulty_reward_coef",
         "eval_interval",
         "num_eval_samples",
         "save_folder",
