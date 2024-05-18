06a43e2029204e2c90b06b8286d1b90c7f438c64
hw@chill <hw56@indiana.edu>
2024-05-18 18:01:34 -0400

    feat: max_new_token 300

--- utils.py
+++ utils.py
@@ -34,7 +34,7 @@ WORD_FREQ_CSV = "word_freq/wiki_token_freq.csv"
 WORD_ACCESSIBILITY_MODEL = "word_freq/model.pkl"
 VOA1500 = 'word_freq/voa1500.json'
 EOS_TOKENS = ['<eos>', '<|endoftext|>', '<\s>']
-MAX_NEW_TOKENS = 512
+MAX_NEW_TOKENS = 300
 
 
 def read_token_frequencies(filename=WORD_FREQ_CSV):
