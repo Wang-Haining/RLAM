c7f1d1e1e426a94f9bc2073d6c2ecddc77cc8f31
hw@chill <hw56@indiana.edu>
2024-06-20 14:11:34 -0400

    feat: add gemma scripts

--- ppo.py
+++ ppo.py
@@ -756,7 +756,7 @@ if __name__ == "__main__":
                 validation_generation_config,
                 num_samples=4  # test a few samples to see if this works
             )
-            validation_score = np.mean(eval_storage["total_scores"])
+            validation_score = eval_storage["total_scores"][0]
             if args.print_sample_output_freq > 0 and (update - 1) % args.print_sample_output_freq == 0:
                 if accelerator.is_main_process:
                     # fixme: no need to save dfs
