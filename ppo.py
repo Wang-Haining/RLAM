fix: dynamic kl control

ppo.py
@@ -1051,11 +1051,13 @@ if __name__ == "__main__":
             accelerator.print("ppo/eps", eps, update)
             # use of dynamic controller
             if args.rluam.target_kl and args.rluam.k_beta:
-                et = np.clip(mean_kl.cpu().numpy() - args.rluam.target_kl - 1, -0.2, 0.2)
-                args.rluam.kl_coef *= 1 + args.rluam.kl_coef * et
+                et = torch.tensor(np.clip(mean_kl.cpu().numpy() - args.rluam.target_kl - 1, -0.2, 0.2), dtype=torch.float32)
+                args.rluam.kl_coef = args.rluam.kl_coef * (1 + args.rluam.kl_coef * et.item())
                 args.rluam.kl_coef = max(args.rluam.kl_coef, 0.0)
             writer.add_scalar("objective/kl_coef", args.rluam.kl_coef, update)
         del kl, mean_kl, mean_entropy, mean_non_score_reward, scores
+        if args.rluam.target_kl and args.rluam.k_beta:
+            del et
         torch.cuda.empty_cache()
 
         if args.run_eval and update % args.eval_steps == 0: