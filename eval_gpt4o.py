d6fbeec449787a7e764c26302b6668b385bb5193
hw@chill <hw56@indiana.edu>
2024-06-28 15:57:03 -0400

    feat: make the pipeline more robust

--- eval_gpt4o.py
+++ eval_gpt4o.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
     MODEL = 'gpt-4o-2024-05-13'
     # system prompt adopted from
     # https://platform.openai.com/docs/guides/text-generation/json-mode
-    SYSTEM_PROMPT = 'You are a helpful assistant designed to output JSON with a single key "abstract".'
+    SYSTEM_PROMPT = 'You are a helpful assistant designed to output JSON.'
 
     ds = load_from_disk(DATASET_PATH)
     generated_texts = []
@@ -116,7 +116,7 @@ if __name__ == "__main__":
             seed=SEED,
             n=1
         )
-        generated_text = json.loads(response.choices[0].message.content)['abstract']
+        generated_text = json.loads(response.choices[0].message.content).values().pop()
         generated_texts.append(generated_text)
 
     # evaluate the generated texts using the function `calculate_metrics`
