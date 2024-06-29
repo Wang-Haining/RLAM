a1d62b6de00ac0efc65fede0ebf77ee022fd0294
hw@chill <hw56@indiana.edu>
2024-06-29 12:16:30 -0400

    feat: lower down val ari lower bound

--- eval_outputs.py
+++ eval_outputs.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
         description="Evaluate SFT and policy model outputs given model type and validation ARI.")
     parser.add_argument("--model", type=str, choices=["gemma-2b", "gemma-7b", "olmo-1b", "llama3-8b", "gpt2-xl", 'phi2-3b'], help="The model type (across runs) to evaluate")
     parser.add_argument("--upper_ari_bound", type=float, default=15.0, help="The upper bound of evaluation ARI for a checkpoint to be considered in the evaluation")
-    parser.add_argument("--lower_ari_bound", type=float, default=10.0, help="The lower bound of evaluation ARI for a checkpoint to be considered in the evaluation")
+    parser.add_argument("--lower_ari_bound", type=float, default=8.0, help="The lower bound of evaluation ARI for a checkpoint to be considered in the evaluation")
     parser.add_argument("--reward", type=str, default='uam', choices=['uam', 'ari'], help="Reward type")
     args = parser.parse_args()
     set_seed(SEED)
