62bd02746aca0f2cd6b9a6801304a8202e28e629
hw@chill <hw56@indiana.edu>
2024-06-29 03:15:51 -0400

    feat: use temp 0.7

--- eval_outputs.py
+++ eval_outputs.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
     # define the generation configuration
     test_generation_config = GenerationConfig(
         max_new_tokens=MAX_OUTPUT_LENGTHS[args.model],
-        temperature=(0.01 + 1e-7),
+        temperature=0.7 + 1e-7,
         top_k=0.0,
         top_p=1.0,
         do_sample=True,
