58c1dee848697606ace40a97f459b0a2bc511fa8
hw@chill <hw56@indiana.edu>
2024-07-03 19:15:43 -0400

    fix: add a small number to prevent divide by 10 when log10

--- eval_outputs.py
+++ eval_outputs.py
@@ -88,7 +88,7 @@ def calculate_metrics(generated_text: str,
                                                                        wa_model,
                                                                        total_tokens,
                                                                        token_freq))
-    p = num_voa_words / num_words
+    p = num_voa_words / num_words + 1e-12
     metrics_dict.update({"voa_log_ratio": np.log(p / (1 - p))})
     metrics_dict.update({"avg_sent_len": np.mean(sent_len_list)})
     metrics_dict.update({"avg_word_accessibility": np.mean(word_accessibility_list)})
